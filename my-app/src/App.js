97d90b6ebec208a3d4cf4c6a6846190536653034
moonmonk <a.wilson1000@gmail.com>
2019-09-20 23:49:28 -0500

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome() {
+  return (        <ul>     
+  <a href='#'>one</a>
+  <a>two</a>
+  <a>three</a>
+  </ul>);
+}
+
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
+
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ul>
-        <p>one</p>
-        <p>two</p>
-        <p>three</p>
-        </ul>
+
+
+        <div>
+          <Welcome/>
+          <ActionLink />
+        </div>
         
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
